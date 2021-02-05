4521a7c8176a01a52c58f341576338b2e3c9c43f
wuq013 <Magnus.Matre@student.uib.no>
2021-02-05 11:15:45 +0100

    Laget brett

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,18 +4,47 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+    private TiledMap map;
+    private TiledMapTileLayer board;
+    private TiledMapTileLayer hole;
+    private TiledMapTileLayer flag;
+    private OrthogonalTiledMapRenderer rend;
+    private OrthographicCamera cam;
+
+
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+
+        map = new TmxMapLoader().load("example.tmx");
+        board = (TiledMapTileLayer) map.getLayers().get("Board");
+        hole = (TiledMapTileLayer) map.getLayers().get("Hole");
+        flag = (TiledMapTileLayer) map.getLayers().get("Flag");
+
+        cam = new OrthographicCamera();
+        rend = new OrthogonalTiledMapRenderer(map, (float) 1/300);
+
+        cam.setToOrtho(false, 5,5);
+        cam.position.set(cam.viewportWidth/2, cam.viewportHeight/2, 0);
+        cam.update();
+
+        rend.setView(cam);
     }
 
     @Override
@@ -28,10 +57,8 @@ public class HelloWorld implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        rend.render();
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
     }
 
     @Override
