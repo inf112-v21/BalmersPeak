3cc97740981cd98a9ca9e05c0c8b58b2324f8ddc
vehjelmtvedt <vetle@hjelmtvedt.com>
2021-02-08 14:44:30 +0100

    fix pom and folders to reflect project name

--- src/main/java/inf112/BalmersPeak/app/Main.java
+++ src/main/java/inf112/BalmersPeak/app/Main.java
@@ -1,4 +1,4 @@
-package inf112.skeleton.app;
+package inf112.BalmersPeak.app;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
