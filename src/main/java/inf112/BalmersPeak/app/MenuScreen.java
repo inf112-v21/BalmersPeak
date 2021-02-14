b2eea33c35b579c466e0faf45539562d379cbedf
vehjelmtvedt <vetle@hjelmtvedt.com>
2021-02-14 15:23:45 +0100

    add background image to menu screen

--- src/main/java/inf112/BalmersPeak/app/MenuScreen.java
+++ src/main/java/inf112/BalmersPeak/app/MenuScreen.java
@@ -1,13 +1,16 @@
 package inf112.balmerspeak.app;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MenuScreen implements Screen {
     private GUI game;
     private SpriteBatch batch;
     private Texture img;
+    private TextureRegion mainBackground;
 
 
     private float f = 0;
@@ -18,20 +21,21 @@ public class MenuScreen implements Screen {
 
     @Override
     public void show() {
+        img = new Texture("menubackground.jpg");
+        mainBackground = new TextureRegion(img, 0, 0, 2000, 2000);
         batch = new SpriteBatch();
-        img = new Texture("logo.jpg");
     }
 
     @Override
     public void render(float v) {
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(mainBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
-        f += v;
-        // Change after 10 seconds
-        if (f > 10) {
-            game.changeScreen(new GameScreen(game));
-        }
+//        f += v;
+//        // Change after 10 seconds
+//        if (f > 10) {
+//            game.changeScreen(new GameScreen(game));
+//        }
     }
 
     @Override
