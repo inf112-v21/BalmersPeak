7e3fdce7ac19991609cee8c1c7ba46a8adbacaff
Philip Perhult Johnsen <philip.johnsen@student.uib.no>
2021-02-17 15:43:02 +0100

    Made a potential setup for cards.

---
+++ src/main/java/inf112/BalmersPeak/app/ICards.java
@@ -0,0 +1,16 @@
+package inf112.balmerspeak.app;
+
+public interface ICards {
+
+    /**
+     * Gets the priority value for the card
+     * @return
+     */
+    int getPriority();
+
+    /**
+     * Gets the action value for the card
+     * @return
+     */
+    int getAction();
+}
