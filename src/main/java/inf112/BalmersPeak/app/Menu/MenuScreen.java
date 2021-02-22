6d5f5af4507527568460b1b187ba5c841f4c1e45
vehjelmtvedt <vetle@hjelmtvedt.com>
2021-02-22 16:17:40 +0100

    changed the way the application exits

--- src/main/java/inf112/BalmersPeak/app/Menu/MenuScreen.java
+++ src/main/java/inf112/BalmersPeak/app/Menu/MenuScreen.java
@@ -1,5 +1,6 @@
 package inf112.balmerspeak.app.menu;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -78,7 +79,7 @@ public class MenuScreen extends MainScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
-                System.exit(0);
+                Gdx.app.exit();
             }
         });
     }
