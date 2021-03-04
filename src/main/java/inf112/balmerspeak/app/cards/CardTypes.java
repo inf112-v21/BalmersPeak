2c71d8c49ba29f21117b88a926076d537fa8d535
Simen Forland <simenforland@yahoo.com>
2021-03-04 11:07:11 +0100

    Fikset namnene på pakkene

---
+++ src/main/java/inf112/balmerspeak/app/cards/CardTypes.java
@@ -0,0 +1,28 @@
+package inf112.balmerspeak.app.cards;
+
+public enum CardTypes {
+
+    //TODO:Legge til i Cards slik at man får en funksjonalitet knyttet til kortene
+    move_one,
+    move_two,
+    move_three,
+    move_back,
+    rotate_left,
+    rotate_right,
+    u_turn;
+    }
+
+/*    public Cards move_one = new Cards(MovementType.movement, 0);
+
+    public Cards move_two = new Cards(MovementType.movement, 0);
+
+    public Cards move_three = new Cards(MovementType.movement, 0);
+
+    public Cards move_back = new Cards(MovementType.movement, 0);
+
+    public Cards turn_right = new Cards(MovementType.rotation, 0);
+
+    public Cards turn_left = new Cards(MovementType.rotation, 0);
+
+    public Cards u_turn = new Cards(MovementType.rotation, 0);*/
+
